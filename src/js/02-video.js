1b2e6ca9ba525ef8fea875a9db568c1e2ebdae4d
mark savka <mark.savka.it@gmail.com>
2022-07-01 14:36:00 +0300

    completed second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,30 @@
 import Vimeo from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Vimeo(iframe, { autoplay: true });
 
-player.on('play', function () {
-  console.log('played the video!');
+document.addEventListener('DOMContentLoaded', function (event) {
+  const s = localStorage.getItem('s');
+  console.log('localStorage: ', s);
+
+  player.play(); // not work, GOOGLE rules
+
+  if (s) player.setCurrentTime(s);
 });
+
+setInterval(() => {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      // seconds = the current playback position
+      localStorage.setItem('s', seconds);
+      console.log(seconds);
+    })
+    .catch(function (error) {
+      console.log('getCurrentTime error', error);
+      // an error occurred
+    });
+}, 1000);
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
