41cdf9fba0c706cb49670ca5d4a831723916b624
mark savka <mark.savka.it@gmail.com>
2022-07-11 19:45:53 +0300

    completed last task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+const LOCALSTORAGEITEM = 'form';
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input[name="email"]');
+const inputMessage = document.querySelector('textarea[name="message"]');
 
+if (!localStorage.getItem(LOCALSTORAGEITEM)) {
+  localStorage.setItem(LOCALSTORAGEITEM, JSON.stringify({}));
+}
+
+let copyOfLocalStorage = JSON.parse(
+  localStorage.getItem(LOCALSTORAGEITEM) ?? {}
+);
+
+if (Object.keys(copyOfLocalStorage).length) insertTextInInputs();
+
+form.addEventListener('input', throttle(onInputForm, 200));
+form.addEventListener('submit', throttle(onSubmitForm, 200));
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  if (copyOfLocalStorage.email.length && copyOfLocalStorage.message.length) {
+    form.reset();
+    copyOfLocalStorage = {};
+    updateLocalStorage();
+  } else alert('Не всі поля заповнені');
+}
+
+function onInputForm(e) {
+  copyOfLocalStorage[e.target.name] = e.target.value;
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem(LOCALSTORAGEITEM, JSON.stringify(copyOfLocalStorage));
+}
+
+function insertTextInInputs() {
+  inputEmail.value = copyOfLocalStorage.email || '';
+  inputMessage.value = copyOfLocalStorage.message || '';
+}
